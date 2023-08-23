ffd71fa52850ed125d42918ef7008094a800d4b8
Stefan Schubert <48105926+stefan-schubert@users.noreply.github.com>
2023-08-23 23:44:53 +0200

    fixes

--- test.js
+++ test.js
@@ -12,12 +12,8 @@ customElements.define(
     }
 
     async init() {
-      //   const devices = await Html5Qrcode.getCameras();
-      //   const device =
-      //     devices.find((d) => d.label.toLowerCase().includes('back')) ??
-      //     devices[devices.length - 1];
       this.querySelector('[data-toggle]').addEventListener('click', () => {
-        this.querySelector('#reader').hidden = false;
+        this.querySelector('[data-scanner]').hidden = false;
         this.querySelector('[data-toggle]').hidden = true;
         this.querySelector('[data-result]').hidden = true;
       });
@@ -38,7 +34,7 @@ customElements.define(
     render(decodedText) {
       const [url] = decodedText.split('?');
       if (validUrls.includes(url)) {
-        this.querySelector('#reader').hidden = true;
+        this.querySelector('[data-scanner]').hidden = true;
         this.querySelector('[data-toggle]').hidden = false;
         const resultCnt = this.querySelector('[data-result]');
         resultCnt.innerHTML = `<img style="width:100%;object-fit: cover;" src="${decodedText}">`;
